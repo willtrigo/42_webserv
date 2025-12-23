fdb78fa13041def30b4dedfbe8cf138e1f441dcf
willtrigo <willtrigo@gmail.com>
2025-12-23 01:19:42 -0300

    Fix: move static const from private to public  Changes to be committed: 	modified:   src/domain/value_objects/Permission.hpp

--- src/domain/value_objects/Permission.hpp
+++ src/domain/value_objects/Permission.hpp
@@ -6,7 +6,7 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/21 00:07:43 by dande-je          #+#    #+#             */
-/*   Updated: 2025/12/21 00:50:25 by dande-je         ###   ########.fr       */
+/*   Updated: 2025/12/22 14:23:29 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,51 @@ class Permission {
   enum Class { CLASS_OWNER, CLASS_GROUP, CLASS_OTHER, CLASS_ALL, CLASS_COUNT };
 
   enum Mode { MODE_READ, MODE_WRITE, MODE_EXECUTE, MODE_COUNT };
+
+  static const unsigned int MIN_PERMISSION = 0;
+  static const unsigned int MAX_PERMISSION = 07777;
+
+  static const unsigned int READ_BIT = 4;
+  static const unsigned int WRITE_BIT = 2;
+  static const unsigned int EXECUTE_BIT = 1;
+
+  static const unsigned int NO_PERMISSION = 0;
+  static const unsigned int READ_ONLY = 0444;
+  static const unsigned int WRITE_ONLY = 0222;
+  static const unsigned int EXECUTE_ONLY = 0111;
+  static const unsigned int READ_WRITE = 0666;
+  static const unsigned int READ_EXECUTE = 0555;
+  static const unsigned int WRITE_EXECUTE = 0333;
+  static const unsigned int ALL_PERMISSIONS = 0777;
+
+  static const unsigned int SETUID_BIT = 04000;
+  static const unsigned int SETGID_BIT = 02000;
+  static const unsigned int STICKY_BIT = 01000;
+
+  static const unsigned int OWNER_READ_WRITE_EXECUTE = 0700;
+  static const unsigned int OWNER_READ_WRITE_GROUP_READ = 0640;
+  static const unsigned int OWNER_READ_WRITE_GROUP_READ_OTHER_READ = 0644;
+  static const unsigned int OWNER_ALL_GROUP_READ_EXECUTE_OTHER_READ_EXECUTE =
+      0755;
+
+  static const std::size_t SYMBOLIC_STRING_LENGTH = 9;
+  static const std::size_t SYMBOLIC_STRING_WITH_SPECIAL_LENGTH = 10;
+  static const std::size_t SPECIAL_BITS_START_INDEX = 3;
+  static const std::size_t OWNER_PERMISSION_SHIFT = 6;
+  static const std::size_t GROUP_PERMISSION_SHIFT = 3;
+  static const std::size_t OTHER_PERMISSION_SHIFT = 0;
+
+  static const std::size_t OWNER_READ_POSITION = 0;
+  static const std::size_t OWNER_WRITE_POSITION = 1;
+  static const std::size_t OWNER_EXECUTE_POSITION = 2;
+  static const std::size_t GROUP_READ_POSITION = 3;
+  static const std::size_t GROUP_WRITE_POSITION = 4;
+  static const std::size_t GROUP_EXECUTE_POSITION = 5;
+  static const std::size_t OTHER_READ_POSITION = 6;
+  static const std::size_t OTHER_WRITE_POSITION = 7;
+  static const std::size_t OTHER_EXECUTE_POSITION = 8;
+
+  static const int BASE_OCTAL = 8;
 
   Permission();
   explicit Permission(unsigned int octalPermission);
@@ -74,51 +119,6 @@ class Permission {
 
  private:
   unsigned int m_permission;
-
-  static const unsigned int MIN_PERMISSION = 0;
-  static const unsigned int MAX_PERMISSION = 07777;
-
-  static const unsigned int READ_BIT = 4;
-  static const unsigned int WRITE_BIT = 2;
-  static const unsigned int EXECUTE_BIT = 1;
-
-  static const unsigned int NO_PERMISSION = 0;
-  static const unsigned int READ_ONLY = 0444;
-  static const unsigned int WRITE_ONLY = 0222;
-  static const unsigned int EXECUTE_ONLY = 0111;
-  static const unsigned int READ_WRITE = 0666;
-  static const unsigned int READ_EXECUTE = 0555;
-  static const unsigned int WRITE_EXECUTE = 0333;
-  static const unsigned int ALL_PERMISSIONS = 0777;
-
-  static const unsigned int SETUID_BIT = 04000;
-  static const unsigned int SETGID_BIT = 02000;
-  static const unsigned int STICKY_BIT = 01000;
-
-  static const unsigned int OWNER_READ_WRITE_EXECUTE = 0700;
-  static const unsigned int OWNER_READ_WRITE_GROUP_READ = 0640;
-  static const unsigned int OWNER_READ_WRITE_GROUP_READ_OTHER_READ = 0644;
-  static const unsigned int OWNER_ALL_GROUP_READ_EXECUTE_OTHER_READ_EXECUTE =
-      0755;
-
-  static const std::size_t SYMBOLIC_STRING_LENGTH = 9;
-  static const std::size_t SYMBOLIC_STRING_WITH_SPECIAL_LENGTH = 10;
-  static const std::size_t SPECIAL_BITS_START_INDEX = 3;
-  static const std::size_t OWNER_PERMISSION_SHIFT = 6;
-  static const std::size_t GROUP_PERMISSION_SHIFT = 3;
-  static const std::size_t OTHER_PERMISSION_SHIFT = 0;
-
-  static const std::size_t OWNER_READ_POSITION = 0;
-  static const std::size_t OWNER_WRITE_POSITION = 1;
-  static const std::size_t OWNER_EXECUTE_POSITION = 2;
-  static const std::size_t GROUP_READ_POSITION = 3;
-  static const std::size_t GROUP_WRITE_POSITION = 4;
-  static const std::size_t GROUP_EXECUTE_POSITION = 5;
-  static const std::size_t OTHER_READ_POSITION = 6;
-  static const std::size_t OTHER_WRITE_POSITION = 7;
-  static const std::size_t OTHER_EXECUTE_POSITION = 8;
-
-  static const int BASE_OCTAL = 8;
 
   void validate() const;
   static unsigned int parsePermissionString(
