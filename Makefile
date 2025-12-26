fbc211d03d6e2cb63e3d5ffd1c20ae7582029b3f
willtrigo <willtrigo@gmail.com>
2025-12-25 21:41:37 -0300

    Feat: implement Host in the domain layer  Changes to be committed: 	modified:   Makefile 	new file:   src/domain/value_objects/Host.cpp 	new file:   src/domain/value_objects/Host.hpp 	new file:   src/shared/exceptions/HostException.cpp 	new file:   src/shared/exceptions/HostException.hpp

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/12/12 17:14:28 by dande-je          #+#    #+#              #
-#    Updated: 2025/12/25 21:36:58 by dande-je         ###   ########.fr        #
+#    Updated: 2025/12/25 21:39:59 by dande-je         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -74,6 +74,7 @@ SRCS_FILES                      += $(addprefix $(SRCS_ENTITIES_DIR), CgiConfig.c
 																	 LocationConfig.cpp \
 																	 Route.cpp)
 SRCS_FILES                      += $(addprefix $(SRCS_VALUE_OBJECTS_DIR), ErrorCode.cpp \
+																	 Host.cpp \
 																	 HttpMethod.cpp \
 																	 Path.cpp \
 																	 Permission.cpp \
@@ -106,6 +107,7 @@ SRCS_FILES                      += $(addprefix $(SRCS_EXCEPTIONS_DIR), BaseExcep
 																	 ErrorCodeException.cpp \
 																	 FileHandlerException.cpp \
 																	 FileSystemHelperException.cpp \
+																	 HostException.cpp \
 																	 HttpMethodException.cpp \
 																	 LocationConfigException.cpp \
 																	 PathException.cpp \
---
+++ src/shared/exceptions/HostException.hpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   HostException.hpp                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/24 14:45:49 by dande-je          #+#    #+#             */
+/*   Updated: 2025/12/24 14:45:51 by dande-je         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HOST_EXCEPTION_HPP
+#define HOST_EXCEPTION_HPP
+
+#include "shared/exceptions/BaseException.hpp"
+
+namespace shared {
+namespace exceptions {
+
+class HostException : public BaseException {
+ public:
+  enum ErrorCode {
+    EMPTY_HOST,
+    INVALID_FORMAT,
+    TOO_LONG,
+    INVALID_IPV4,
+    INVALID_IPV6,
+    INVALID_HOSTNAME,
+    MALFORMED,
+    CODE_COUNT
+  };
+
+  explicit HostException(const std::string& msg, ErrorCode code);
+  HostException(const HostException& other);
+  virtual ~HostException() throw();
+
+  HostException& operator=(const HostException& other);
+
+ private:
+  static const std::pair<ErrorCode, std::string> K_CODE_MSGS[];
+
+  static std::string getErrorMsg(ErrorCode code);
+};
+
+}  // namespace exceptions
+}  // namespace shared
+
+#endif  // HOST_EXCEPTION_HPP
